74161e9d084a4e48f6c91b92cfa3966b86e24e55
saraheulie <saraheulie@creighton.edu>
2021-04-29 10:09:24 -0500

    delete duration

--- forms/RealDiet/RealDiet.js
+++ forms/RealDiet/RealDiet.js
@@ -6,7 +6,7 @@ btnSumbit.onclick = function() {
     let Carbs = inptcarbs.value
     let foodType = inptfoodType.value
     let currentDate = inptcurrentDate.value
-    let query = "INSERT INTO diet (`Calories`,`Meal_Type`, `user_id`, `DATE`, `Fats`, `Carbs`, `Protein`) VALUES ('" + calories + "', '" + mealType + "', '" + user_id + "','" + currentDate + "', '" + Fats + "', '" + Carbs + "', '" + Protein + "')"
+    let query = "INSERT INTO diet (`Calories`,`Meal_Type`, `user_id`, `DATE`, `Fats`, `Carbs`, `Protein`, `Food_Type`) VALUES (" + calories + ", '" + mealType + "', " + user_id + ",'" + currentDate + "', " + Fats + ", " + Carbs + ", " + Protein + ",' " + foodType + "')"          
 
 
     req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=375groupa2&query=" + query)
