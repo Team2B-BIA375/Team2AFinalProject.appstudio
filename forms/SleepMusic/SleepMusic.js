b36eca3b0788b8b77248945a96d3adbc8c87e8c1
saraheulie <saraheulie@creighton.edu>
2021-05-02 15:16:17 -0500

    updates

--- forms/SleepMusic/SleepMusic.js
+++ forms/SleepMusic/SleepMusic.js
@@ -16,7 +16,7 @@ btnMusic2.onclick=function(){
   //const searchTerms = ["factory%20functions","data%20structures", "array%20functions%20javascript", "composition%20over%20inheritance", "lambda%20functions", "streams%20java", "higher%20order%functions%javascript", "functional%20programming", "c++%20lambda%20functions", "sorting%20algorithms"];
   //const getSearchTerm = () => searchTerms[Math.floor(Math.random() * (searchTerms.length-1))];
   //const YOUTUBE_API_KEY = "";
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDQDqbT0MqzwqDkCTikF0boH4dp4IyVsdg&q=relaxing music&maxResults=1`;
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDQDqbT0MqzwqDkCTikF0boH4dp4IyVsdg&q=chill music&maxResults=1`;
   // console.log(url);
   fetch(url)
     .then(response => response.json())
